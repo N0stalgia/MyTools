23c26679f3692129679bd55b63295646c42cebd7
N0stalgia <735700884@qq.com>
2018-05-07 10:38:58 +0800

    may02-05

---
+++ host.java
@@ -0,0 +1,6 @@
+	192.168.1.181	nap-api                 #nap-api
+	192.168.1.181	nap-postgres-host	#datasource
+	192.168.1.181	nap-mongodb-host	#mongodb
+	192.168.1.181	nap-rabbitmq-host	#rabbitmq
+	192.168.1.181	nap-es-host		#elasticsearch	 
+	0.0.0.0         account.jetbrains.com   #Intellij IDEA			
