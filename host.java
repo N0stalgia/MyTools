d160e5ec7e6027622a1a65f17cd9e01103842e6b
N0stalgia <735700884@qq.com>
2018-07-10 23:49:42 +0800

    Update host.java

--- host.java
+++ host.java
@@ -3,4 +3,7 @@
 	192.168.1.181	nap-mongodb-host	#mongodb
 	192.168.1.181	nap-rabbitmq-host	#rabbitmq
 	192.168.1.181	nap-es-host		#elasticsearch	 
-	0.0.0.0         account.jetbrains.com   #Intellij IDEA			
+	0.0.0.0         account.jetbrains.com   #Intellij IDEA		
+	   
+	   依照策略进行配置nat，如要允许内网的某个网段访问外网进行
+	   设备上有多个公网地址时，选哪个？   确保配置了NAT的设备能够收到从公网来的数据包，或者从私网出去的数据包   
